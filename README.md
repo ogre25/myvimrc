79f3538798846960a7d37b04875ba4b64e3a30cf
ogre25 <ogre25@126.com>
2019-08-28 12:30:35 +0800

    vimsetup shell update

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # myvimrc
-simple vimrc
+a simple vimrc
+
+Include plugins:
+1、nerdtree
+2、auto-pairs
+Hotkey    
+1、F3 mirror/toggle nerdtree
+2、F5 one key run bash/python3 code
+
--- vimsetup.sh
+++ vimsetup.sh
@@ -2,7 +2,7 @@
 ho=$HOME/.vim/bundle
 if [ ! -e $ho ];then
     mkdir $ho
+    git clone https://github.com/VundleVim/Vundle.vim.git $ho/Vundle.vim && cp vimrc $HOME/.vimrc && vim +PluginInstall +qall
+else
+    git clone https://github.com/VundleVim/Vundle.vim.git $ho/Vundle.vim && cp vimrc $HOME/.vimrc && vim +PluginInstall +qall
 fi
-git clone https://github.com/VundleVim/Vundle.vim.git $ho/Vundle.vim
-cp vimrc $HOME/.vimrc
-vim +PluginInstall +qall
