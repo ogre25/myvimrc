0a2c3a36a330597e7fe5fa07a44331675a0daf09
ogre25 <ogre25@126.com>
2019-09-02 12:18:00 +0800

    include youcomplete and remove other line

--- vimsetup.sh
+++ vimsetup.sh
@@ -2,7 +2,9 @@
 ho=$HOME/.vim/bundle
 if [ ! -e $ho ];then
     mkdir $ho
-    git clone https://github.com/VundleVim/Vundle.vim.git $ho/Vundle.vim && cp vimrc $HOME/.vimrc && vim +PluginInstall +qall
+    git clone https://github.com/VundleVim/Vundle.vim.git $ho/Vundle.vim && cp vimrc $HOME/.vimrc
+    git clone https://github.com/ycm-core/YouCompleteMe.git $ho/YouCompleteMe && cd $ho/YouCompleteMe && git submodule update --init --recursive && python3 install.py --clang-completer && vim +PluginInstall +qall
 else
-    git clone https://github.com/VundleVim/Vundle.vim.git $ho/Vundle.vim && cp vimrc $HOME/.vimrc && vim +PluginInstall +qall
+    git clone https://github.com/VundleVim/Vundle.vim.git $ho/Vundle.vim && cp vimrc $HOME/.vimrc
+    git clone https://github.com/ycm-core/YouCompleteMe.git $ho/YouCompleteMe && cd $ho/YouCompleteMe && git submodule update --init --recursive && python3 install.py --clang-completer && vim +PluginInstall +qall
 fi
