7ef608cb748faefec79704bd0738338a56ad9fd3
ogre25 <ogre25@126.com>
2019-09-04 14:22:09 +0800

    add jedi-vim

--- vimsetup-include-youcompleteme.sh
+++ vimsetup-include-youcompleteme.sh
@@ -3,8 +3,8 @@ ho=$HOME/.vim/bundle
 if [ ! -e $ho ];then
     mkdir $ho
     git clone https://github.com/VundleVim/Vundle.vim.git $ho/Vundle.vim && cp vimrc $HOME/.vimrc
-    git clone https://github.com/ycm-core/YouCompleteMe.git $ho/Vundle.vim/YouCompleteMe && git submodule update --init --recursive && cd $ho/Vundle.vim/YouCompleteMe && python3 install.py --clang-completer && vim +PluginInstall +qall
+    git clone https://github.com/ycm-core/YouCompleteMe.git $ho/Vundle.vim/YouCompleteMe && git submodule update --init --recursive && cd $ho/Vundle.vim/YouCompleteMe && python3 install.py --clang-completer && vim +PluginInstall +qall && pip3 install jedi
 else
     git clone https://github.com/VundleVim/Vundle.vim.git $ho/Vundle.vim && cp vimrc $HOME/.vimrc
-    git clone https://github.com/ycm-core/YouCompleteMe.git $ho/Vundle.vim/YouCompleteMe && git submodule update --init --recursive && cd $ho/Vundle.vim/YouCompleteMe && python3 install.py --clang-completer && vim +PluginInstall +qall
+    git clone https://github.com/ycm-core/YouCompleteMe.git $ho/Vundle.vim/YouCompleteMe && git submodule update --init --recursive && cd $ho/Vundle.vim/YouCompleteMe && python3 install.py --clang-completer && vim +PluginInstall +qall && pip3 install jedi
 fi
