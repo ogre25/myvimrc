b387b0001959795e8492b303988c1b68ae9c62d0
ogre25 <ogre25@126.com>
2019-09-09 10:45:08 +0800

    sh bug fix

--- vimsetup-include-youcompleteme-jedi.sh
+++ vimsetup-include-youcompleteme-jedi.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 ho=$HOME/.vim/bundle
+vun=$(git clone https://github.com/VundleVim/Vundle.vim.git $ho/Vundle.vim && cp vimrc-include-youcompleteme-jedi $HOME/.vimrc)
+you=$(git clone https://github.com/ycm-core/YouCompleteMe.git $ho/YouCompleteMe && cd $ho/YouCompleteMe && git submodule update --init --recursive && python3 install.py --clang-completer && vim +PluginInstall +qall)
 if [ ! -e $ho ];then
     mkdir $ho
-    git clone https://github.com/VundleVim/Vundle.vim.git $ho/Vundle.vim && cp vimrc-include-youcompleteme-jedi $HOME/.vimrc
-    git clone https://github.com/ycm-core/YouCompleteMe.git $ho/YouCompleteMe && cd $ho/YouCompleteMe && git submodule update --init --recursive && python3 install.py --clang-completer && vim +PluginInstall +qall
+    $vun
+    $you
 else
-    git clone https://github.com/VundleVim/Vundle.vim.git $ho/Vundle.vim && cp vimrc-include-youcompleteme-jedi $HOME/.vimrc
-    git clone https://github.com/ycm-core/YouCompleteMe.git $ho/YouCompleteMe && cd $ho/YouCompleteMe && git submodule update --init --recursive && python3 install.py --clang-completer && vim +PluginInstall +qall
+    $vun
+    $you
 fi
--- vimsetup.sh
+++ vimsetup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 ho=$HOME/.vim/bundle
+vun=$(git clone https://github.com/VundleVim/Vundle.vim.git $ho/Vundle.vim && cp vimrc $HOME/.vimrc)
 if [ ! -e $ho ];then
     mkdir $ho
-    git clone https://github.com/VundleVim/Vundle.vim.git $ho/Vundle.vim && cp vimrc $HOME/.vimrc
-    git clone https://github.com/ycm-core/YouCompleteMe.git $ho/YouCompleteMe && cd $ho/YouCompleteMe && git submodule update --init --recursive && python3 install.py --clang-completer && vim +PluginInstall +qall
+    $vun
 else
-    git clone https://github.com/VundleVim/Vundle.vim.git $ho/Vundle.vim && cp vimrc $HOME/.vimrc
-    git clone https://github.com/ycm-core/YouCompleteMe.git $ho/YouCompleteMe && cd $ho/YouCompleteMe && git submodule update --init --recursive && python3 install.py --clang-completer && vim +PluginInstall +qall
+    $vun
 fi
