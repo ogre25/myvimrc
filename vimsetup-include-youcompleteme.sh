ec1153cdda4a92f8673493bdaa931fa162f49a4b
ogre25 <ogre25@126.com>
2019-09-11 10:47:53 +0800

    sh update

--- vimsetup-include-youcompleteme.sh
+++ vimsetup-include-youcompleteme.sh
@@ -4,9 +4,6 @@ vun=$(git clone https://github.com/VundleVim/Vundle.vim.git $ho/Vundle.vim && cp
 you=$(git clone https://github.com/ycm-core/YouCompleteMe.git $ho/YouCompleteMe && cd $ho/YouCompleteMe && git submodule update --init --recursive && python3 install.py --clang-completer && vim +PluginInstall +qall)
 if [ ! -e $ho ];then
     mkdir $ho
-    $vun
-    $you
-else
-    $vun
-    $you
 fi
+$vun
+$you
--- vimsetup.sh
+++ vimsetup.sh
@@ -3,7 +3,5 @@ ho=$HOME/.vim/bundle
 vun=$(git clone https://github.com/VundleVim/Vundle.vim.git $ho/Vundle.vim && cp vimrc $HOME/.vimrc)
 if [ ! -e $ho ];then
     mkdir $ho
-    $vun
-else
-    $vun
 fi
+$vun
