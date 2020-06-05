ae0d0dad5fd43ae33a4b2990495c4706caac5281
ogre25 <ogre25@126.com>
2020-06-05 15:48:51 +0800

    a lot bug fix

--- vimsetup-include-youcompleteme.sh
+++ vimsetup-include-youcompleteme.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ho=$HOME/.vim/bundle
 vun=$(git clone https://github.com/VundleVim/Vundle.vim.git $ho/Vundle.vim && cp vimrc-include-youcompleteme $HOME/.vimrc)
-you=$(git clone https://github.com/ycm-core/YouCompleteMe.git $ho/YouCompleteMe && cd $ho/YouCompleteMe && git submodule update --init --recursive && python3 install.py --clang-completer && vim +PluginInstall +qall)
+you=$(git clone https://github.com/ycm-core/YouCompleteMe.git $ho/YouCompleteMe && cd $ho/YouCompleteMe && git submodule update --init --recursive && sudo python3 install.py --clang-completer && vim -c 'PluginInstall' -c 'qa!')
 if [ ! -e $ho ];then
     mkdir $ho
 fi
